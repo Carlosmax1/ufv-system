ebdfe727d8ec12bfba686f125a4fd91ce3488bcd
Carlosmax1 <carlosjogosbr@gmail.com>
2022-07-26 13:51:36 -0300

    Criação do Middleware

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { status } from "./controllers/StatusController";
 import { createLAE } from "./controllers/CreateLAE";
 import { GetLAE } from "./controllers/GetLAE";
+import { Auth } from "./middleware/middleware";
 
 export const routes = express.Router();
 
 routes.get('/', status);
-routes.post('/api/v1/create', createLAE);
+routes.post('/api/v1/create', Auth, createLAE);
 routes.get('/api/v1/lae/:date', GetLAE); // 'DIA-MES-ANO'
